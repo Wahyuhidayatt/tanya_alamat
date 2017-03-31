19d5eec455dd4b0d8bae82e87d815caf25f24d16
hepiska <ego1403@gmail.com>
2017-03-31 14:24:06 +0700

    rapiin chat

--- server/bot.js
+++ server/bot.js
@@ -13,7 +13,7 @@ bot.on('message', (msg, match) => {
   //console.log(msg.from.id);
   //console.log(msg);
   let chatId = msg.chat.id;
-  Chat.insertChat(msg.from,msg.text,msg.date);
+  //Chat.insertChat(msg.from,msg.text,msg.date);
   // gmaps.places({
   //   query:`${msg.text}`
   // },function(err,data){
@@ -26,5 +26,64 @@ bot.on('message', (msg, match) => {
 // )
 
 });
+
+bot.onText(/\/start/,(msg,match)=>{
+  bot.sendMessage(msg.from.id,`selamat datang di tanya alamat, disini kamu bisa menayakan alamat tempat yang kamu mau
+contoh:
+/cari pondok indah mall untuk mencari alamat pondok indah mall
+/dekat atm untuk mencari atm terdekat`)
+})
+
+bot.onText(/\/cari\s.+/,(msg,match)=>{
+  let chatId = msg.chat.id;
+  let chatText=msg.text.split(' ');
+     chatText.shift();
+     chatText=chatText.join(' ')
+  console.log(chatText);
+
+  Chat.insertChat(msg.from,chatText,msg.date);
+  gmaps.places({
+    query:`${msg.text}`
+  },function(err,data){
+    //console.log(data.json.results[0].geometry.location);
+    bot.sendVenue(chatId,data.json.results[0].geometry.location.lat,
+                  data.json.results[0].geometry.location.lng,
+                  data.json.results[0].name,data.json.results[0].formatted_address)
+
+   }
+)
+});
+
+bot.onText(/\/dekat\s.+/,(msg,match)=>{
+  let chatId = msg.chat.id;
+  let chatText=msg.text.split(' ');
+     chatText.shift();
+     chatText=chatText.join(' ')
+  console.log(chatText);
+
+  Chat.insertChat(msg.from,chatText,msg.date);
+  gmaps.places({
+    query:`${msg.text}`
+  },function(err,data){
+    //console.log(data.json.results[0].geometry.location);
+    bot.sendVenue(chatId,data.json.results[0].geometry.location.lat,
+                  data.json.results[0].geometry.location.lng,
+                  data.json.results[0].name,data.json.results[0].formatted_address)
+
+   }
+)
+});
+
+bot.onText(/\/cari/,(msg,match)=>{
+  if (msg.text.split(' ').length<2) {
+    bot.sendMessage(msg.from.id,`untuk menggunakan menu ini gunakan /cari<space>apa yang dicari`)
+  }
+})
+
+bot.onText(/\/dekat/,(msg,match)=>{
+  if (msg.text.split(' ').length<2) {
+    bot.sendMessage(msg.from.id,`untuk menggunakan menu ini gunakan /dekat<space>apa yang dicari`)
+  }
+})
 
 module.exports = bot
